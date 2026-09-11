fab58ddb5ca9325def5dba7380ec9d763b8fa603
Guy <guylevi@hotmail.com>
2026-09-11 08:24:45 -0500

    Support PORT and BACKEND_PORT environment variables with defaults

--- server/index.js
+++ server/index.js
@@ -3,7 +3,7 @@ import { defaultProgress, readProgress, writeProgress } from './progressStore.js
 import { buildSeed } from './seeds.js'
 
 const app = express()
-const PORT = 3001
+const PORT = process.env.PORT || 3001
 
 app.use(express.json())
 
